e6665f558ac6a348a84633af32d562bebe6cce26
Matilda Idoffsson <matido@student.chalmers.se>
2017-11-14 14:53:16 +0100

    Labb 2

--- src/BinHeap.java
+++ src/BinHeap.java
@@ -1,8 +1,37 @@
-public class BinHeap {
+import java.util.Iterator;
+import java.util.Comparator;
+import java.util.ArrayList;
+
+public class BinHeap <E>implements PrioQueue<E> {
 
     public BinHeap(Comparator<? super E> comp) {
 
     }
+    public void add(E e){
 
+    }
 
+    public E poll(){
+      return null;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+    @Override
+    public void remove(E e) {
+
+    }
+
+    @Override
+    public E peek() {
+        return null;
+    }
+
+    @Override
+    public void remove() {
+        throw new UnsupportedOperationException();
+    }
 }
